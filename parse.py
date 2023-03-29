e87b8347df33c17d129c00e1c96c7b3998b8ff57
Jeff Zhou <jlz45@cornell.edu>
2023-03-29 02:05:30 -0400

    hits

--- parse.py
+++ parse.py
@@ -9,17 +9,21 @@ import math
 
 # %%
 MONTH = 0
-with open("cscareerquestions.json") as fp:
+with open("CryptoCurrency.json") as fp:
     month_nets = json.load(fp)
 
 G = nx.DiGraph()
 
-edge_list = []
 for u in month_nets[MONTH]:
     for v in month_nets[MONTH][u]:
-        if u!=v:
+        if u != v:
             G.add_edge(u, v)
 
 # %%
-nx.draw(G, node_size = 30)
+nx.draw(G, node_size=30)
 plt.show()
+
+# %%
+h, a = nx.hits(G)
+h = dict(sorted(h.items(), key=lambda item: -item[1]))
+a = dict(sorted(a.items(), key=lambda item: -item[1]))
