8ae669e4542876dbab128c9e83952db335835205
kimmylin727 <kimmylin727@gmail.com>
2023-03-29 01:58:56 -0400

    smaller nodes and no self loop

--- parse.py
+++ parse.py
@@ -17,8 +17,9 @@ G = nx.DiGraph()
 edge_list = []
 for u in month_nets[MONTH]:
     for v in month_nets[MONTH][u]:
-        G.add_edge(u, v)
+        if u!=v:
+            G.add_edge(u, v)
 
 # %%
-nx.draw(G)
+nx.draw(G, node_size = 30)
 plt.show()
