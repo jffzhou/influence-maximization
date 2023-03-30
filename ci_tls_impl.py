7ba64e97f7fca1e1207c635363e45bf664e5bddb
Jeff Zhou <jlz45@cornell.edu>
2023-03-30 00:00:12 -0400

    influence maximization algorithm andnd preliminary analysis

--- ci_tls_impl.py
+++ ci_tls_impl.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 import collections
+from copy import deepcopy
 
+# %%
 activated = set()
 beta = 0.5
 
@@ -23,29 +25,27 @@ test_G.add_edges_from(edgelist)
 
 degs = {n: test_G.degree(n) for n in test_G.nodes()}
 
+# %%
+
+
 def activation(u, v, d, neighbors):
     # d is cascade depth NOT distance between u and v
     l = 1/d
-    print(u, v, d, neighbors & activated)
     m = len(neighbors & activated)
     p = 1 - (1 - beta) ** (1 + (m/(degs[u]-1)))
     return p * l
+
 
 def CI_TLS_rec(G, n):
     activated.add(n)
 
     sigmas = {n: 0}  # activation probabilities
 
-    # def sigma(v):
-    #     if v in sigmas:
-    #         return sigmas[v]
-    #     # return 1 - np.prod([1 - ])
-
-    ######################### Calculate RELEVANT nodes ######################### 
-    relevant_nodes = collections.defaultdict(set)
+    ######################### Calculate RELEVANT nodes #########################
+    relevant_nodes = {}
     q = collections.deque()
-    depth_set = [set([n])]
-    visited = set([n])
+    depth_set = [{n}]
+    visited = {n}
 
     q.append(n)
     for d in range(4):
@@ -59,6 +59,7 @@ def CI_TLS_rec(G, n):
                     visited.add(neighbor)
                     depth_set[d+1].add(neighbor)
                     q.append(neighbor)
+
     ############################## Compute sigmas ##############################
     q.clear()
     visited.clear()
@@ -75,7 +76,8 @@ def CI_TLS_rec(G, n):
                 # print(d,v,sigmas)
                 product = 1
                 for r in relevant_nodes[v]:
-                    product *= (1 - sigmas[r] * activation(r, v, d, set(G.neighbors(r))))
+                    product *= (1 - sigmas[r] * activation(r,
+                                v, d, set(G.neighbors(r))))
                 sigmas[v] = 1 - product
             for neighbor in G.neighbors(v):
                 if neighbor not in visited:
@@ -85,7 +87,86 @@ def CI_TLS_rec(G, n):
             # print(node)
             activated.add(node)
 
-    for k, v in sigmas.items():
-        print(k, v)
+    return np.sum(list(sigmas.values())), list(sigmas.keys())
 
-print(CI_TLS_rec(test_G, 9))
+
+for i in range(1, 10):
+    print(f"{i}: {CI_TLS_rec(test_G, i)}")
+
+
+# %% influence maximization algorithm
+def ci_tls_im(G, k):
+    S = set()
+    G_copy = deepcopy(G)
+    while len(S) < k and len(G_copy.nodes()) > 0:
+        # calculate scores
+        ci_tls_scores = []
+        local_nodes = []
+        for n in G_copy.nodes():
+            score, local = CI_TLS_rec(G_copy, n)
+            ci_tls_scores.append(score)
+            local_nodes.append(local)
+
+        # add node with highest score
+        i = list(G_copy.nodes())[np.argmax(ci_tls_scores)]
+        S.add(i)
+
+        # remove nodes within 3 edges of i
+        for n in local_nodes[np.argmax(ci_tls_scores)]:
+            G_copy.remove_node(n)
+
+    return S
+
+
+# %%
+MONTH = 0
+with open("CryptoCurrency.json") as fp:
+    month_nets = json.load(fp)
+
+G = nx.DiGraph()
+
+for u in month_nets[MONTH]:
+    for v in month_nets[MONTH][u]:
+        if u != v:
+            G.add_edge(u, v)
+
+USE_DIRECTED = False
+degs = {}
+if USE_DIRECTED:
+    degs = {n: G.indegree(n) for n in G.nodes()}
+else:
+    G = G.to_undirected(reciprocal=False)
+    degs = {n: G.degree(n) for n in G.nodes()}
+
+# %%
+activated = set()
+beta = 0.05
+k = 20
+seed = ci_tls_im(G, k)
+print(seed)
+
+color = ["red" if n in seed else "grey" for n in G.nodes()]
+nx.draw(G, node_size=10, node_color=color)
+plt.show()
+
+# %% HITS algorithm
+h, a = nx.hits(G)
+h = dict(sorted(h.items(), key=lambda item: -item[1]))
+a = dict(sorted(a.items(), key=lambda item: -item[1]))
+
+top_hubs = list(h.keys())[:k]
+top_auth = list(a.keys())[:k]
+print(f"{k} highest hub scores:")
+for u in top_hubs:
+    print(f"{u}: {h[u]}")
+
+print(f"\n{k} highest authority scores:")
+for u in top_auth:
+    print(f"{u}: {a[u]}")
+
+# %%
+for n in seed:
+    print(f"{n} hub ranking: {list(h.keys()).index(n)}")
+    print(f"\thub score: {h[n]}")
+    print(f"{n} auth ranking: {list(a.keys()).index(n)}")
+    print(f"\tauth score: {a[n]}")
--- parse.py
+++ parse.py
@@ -6,9 +6,11 @@ import itertools
 import scipy
 import matplotlib.pyplot as plt
 import math
+import numpy as np
+import collections
 
 # %%
-MONTH = 0
+MONTH = 1
 with open("CryptoCurrency.json") as fp:
     month_nets = json.load(fp)
 
@@ -23,16 +25,158 @@ for u in month_nets[MONTH]:
 nx.draw(G, node_size=30)
 plt.show()
 
-# %%
+# %% HITS algorithm
 h, a = nx.hits(G)
 h = dict(sorted(h.items(), key=lambda item: -item[1]))
 a = dict(sorted(a.items(), key=lambda item: -item[1]))
 
-n = 5
+n = 10
+top_hubs = list(h.keys())[:n]
+top_auth = list(a.keys())[:n]
 print(f"{n} highest hub scores:")
-for u in list(h.keys())[:n]:
+for u in top_hubs:
     print(f"{u}: {h[u]}")
 
 print(f"\n{n} highest authority scores:")
-for u in list(a.keys())[:n]:
+for u in top_auth:
     print(f"{u}: {a[u]}")
+
+# %% graph of just top hubs and authorities
+nodelist = set(top_hubs + top_auth)
+edgelist = [e for e in G.edges() if e[0] in nodelist and e[1] in nodelist]
+color = [("purple" if n in top_auth else "red") if n in top_hubs
+         else "blue" if n in top_auth else "grey"
+         for n in nodelist]
+reduced_G = nx.DiGraph()
+reduced_G.add_nodes_from(nodelist)
+reduced_G.add_edges_from(edgelist)
+nx.draw_circular(reduced_G, node_color=color)
+plt.show()
+
+
+# %%
+undir_G = G.to_undirected()
+beta = 0.01
+
+# distance dictionary
+dpath = {x[0]: x[1] for x in nx.all_pairs_shortest_path_length(undir_G)}
+
+# degree dictionary
+degs = {n: undir_G.degree(n) for n in undir_G.nodes()}
+
+# activated nodes
+activated = set()
+
+# %%
+
+
+def activation(u, v, d, neighbors):
+    # d is cascade depth NOT distance between u and v
+    l = 1/d
+    m = len(neighbors & activated)
+    p = 1 - (1 - beta) ** (1 + (m/(degs[u]-1)))
+    return p * l
+
+
+def final_activation(i, v, d):
+    if v in undir_G.neighbors(i):
+        return beta
+    neighbors = []
+    return 1 - np.prod([1 - final_activation(i, u)*activation(u, v, d+1) for u in neighbors])
+
+
+def CI_TLS_rec(G, n):
+    activated.add(n)
+
+    sigmas = {n: 0}  # activation probabilities
+
+    def sigma(v):
+        if v in sigmas:
+            return sigmas[v]
+        # return 1 - np.prod([1 - ])
+
+    relevant_nodes = collections.defaultdict(set)
+    current = set()
+
+    def dfs(i, depth):
+        if depth <= 2:
+            current.add(i)
+            for neighbor in G.neighbors(i):
+                if neighbor not in current:
+                    relevant_nodes[neighbor] |= current
+                    dfs(neighbor, depth + 1)
+            current.remove(i)
+
+    # construct depth-set first, do dfs here
+    dfs(n, 0)
+    print(relevant_nodes)
+
+# %%
+
+
+def CI_TLS(G, n):
+    activated.add(n)
+    # do calculation
+    sigmas = {n: 0}
+    queue = []
+
+    for neighbor in G.neighbors(n):
+        sigmas[neighbor] = beta
+        activated.add(neighbor)
+        queue.append((neighbor, 2))
+
+    while queue:
+        u, d = queue.pop(0)
+        for v in G.neighbors(u):
+            if v not in sigmas and d < 4:
+                print(v)
+                previous = sigmas.keys() & G.neighbors(v)
+                for u in previous:
+                    print(f"({u}, {v})")
+                    print(sigmas[u])
+                    print(set(G.neighbors(u)))
+                    print(activated)
+                    print(activation(u, v, d, set(G.neighbors(u))))
+                prod = [
+                    1 - sigmas[u]*activation(u, v, d, set(G.neighbors(neighbor))) for u in previous]
+                print(prod)
+                sigmas[v] = 1 - np.prod(prod)
+                activated.add(v)
+                queue.append((v, d + 1))
+
+    for u in sigmas:
+        activated.remove(u)
+    print(sigmas)
+    return sum(sigmas.values())
+
+
+def CI_TLS_IM(G, k):
+    S = {}
+    CI_TLS_scores = {n: CI_TLS(G, n) for n in G.nodes()}
+
+
+CI_TLS(test_G, 1)
+# %% Construct test graph
+
+test_G = nx.Graph()
+nodelist = range(1, 10)
+edgelist = [[1, 2], [1, 4], [1, 3], [3, 6],
+            [3, 5], [6, 9], [5, 8], [5, 7], [4, 6]]
+
+
+test_G.add_nodes_from(nodelist)
+test_G.add_edges_from(edgelist)
+
+# distance dictionary
+dpath = {x[0]: x[1] for x in nx.all_pairs_shortest_path_length(test_G)}
+
+# degree dictionary
+degs = {n: test_G.degree(n) for n in test_G.nodes()}
+
+beta = 0.5
+
+nx.draw(test_G, with_labels=True)
+plt.show()
+
+# %%
+CI_TLS_rec(test_G, 1)
